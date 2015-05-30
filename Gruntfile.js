477d25021cbcd03a8a0a04993f13e006f2ce469c
Karl Groves <karl@karlgroves.com>
2015-05-30 10:08:40 -0400

    Initial scaffolding of project

---
+++ Gruntfile.js
@@ -0,0 +1,47 @@
+'use strict';
+
+module.exports = function (grunt) {
+    // Load all grunt tasks
+    require('load-grunt-tasks')(grunt);
+
+    // Show elapsed time at the end
+    require('time-grunt')(grunt);
+
+    // Project configuration.
+    grunt.initConfig({
+
+        pkg: grunt.file.readJSON('package.json'),
+
+
+        jshint: {
+            options: {
+                reporter: require('jshint-stylish')
+            },
+            gruntfile: {
+                options: {
+                    jshintrc: '.jshintrc'
+                },
+                src: 'Gruntfile.js'
+            },
+            src: {
+                options: {
+                    jshintrc: 'src/.jshintrc'
+                },
+                src: ['src/**/*.js']
+            }
+        },
+        watch: {
+            gruntfile: {
+                files: '<%= jshint.gruntfile.src %>',
+                tasks: ['jshint:gruntfile']
+            },
+            src: {
+                files: '<%= jshint.src.src %>',
+                tasks: ['jshint:src', 'qunit']
+            }
+        }
+    });
+
+    // Default task.
+    grunt.registerTask('default', ['jshint']);
+};
---
+++ README.md
@@ -0,0 +1,22 @@
+## Project Scaffolding
+
+* Create .bowerrc file
+* Create .editorconfig file
+* Create .gitattributes file
+* Create .gitignore file
+* Create .jshintrc file
+* Create bower.json file
+* Create package.json file
+* Create Gruntfile
+
+## Install Deps
+
+Run `npm install && bower install`
+
+## Develop
+
+* Create Web Page, CSS, and Script for the base of the demo.
+
+
+
+
